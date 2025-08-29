935d4984e844ab7f54146f472d6cb06042f5c215
Princy Jaiswal <154440490+jprincy12@users.noreply.github.com>
2025-08-29 15:02:24 +0545

    Fix Error

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Navbar } from "./components/navbar/Navbar";
+import { Navbar } from "./components/Navbar/Navbar";
 import styles from "./App.module.css";
 import { Hero } from "./components/Hero/Hero";
 import { About } from "./components/About/About";
